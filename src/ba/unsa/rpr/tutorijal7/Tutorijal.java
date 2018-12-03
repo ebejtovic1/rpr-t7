2752027c5d8bcf22a88603679a2d8672f79410b2
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2018-12-04 00:02:48 +0100

    Dodana metoda zapisiXml()

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,6 +1,8 @@
 package ba.unsa.rpr.tutorijal7;
 
+import java.beans.XMLEncoder;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -27,19 +29,47 @@ public class Tutorijal {
             }
             gradovi.add(new Grad(grad, temperature));
         }
-
+        ulaz.close();
         return gradovi;
     }
-    public static void main(String[] args) {
+
+    public static void zapisiXml(UN drzave) throws FileNotFoundException {
+        XMLEncoder izlaz = new XMLEncoder(new FileOutputStream("un.xml"));
+        izlaz.writeObject(drzave);
+        izlaz.close();
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Grad> rez=new ArrayList<Grad>();
 
         try {
-            var rez = ucitajGradove();
+            rez = ucitajGradove();
             for (var grad : rez) {
                 System.out.println(grad);
             }
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
+        Grad g1=rez.get(0);
+        Grad g2=rez.get(1);
+        Drzava drzava=new Drzava();
+        drzava.setGlavniGrad(g1);
+        drzava.setNaziv("Bih");
+        drzava.setBrojStanovnika(45525);
+        drzava.setJedinicaZaPovrsinu("km");
+        drzava.setPovrsina(25525.0);
+
+        Drzava drzava1=new Drzava();
+        drzava1.setGlavniGrad(g2);
+        drzava1.setNaziv("BH");
+        drzava1.setBrojStanovnika(455);
+        drzava1.setJedinicaZaPovrsinu("km");
+        drzava1.setPovrsina(225.0);
+
+        UN drz=new UN();
+        drz.dodajDrzavu(drzava);
+        drz.dodajDrzavu(drzava1);
+        zapisiXml(drz);
+
 
     }
 }
